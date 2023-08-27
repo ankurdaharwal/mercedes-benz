212ae1068f1b0d889241101ad8fff9ef1b06eb2c
Ankur Daharwal <ankur.daharwal@gmail.com>
2023-08-28 03:10:41 +0530

    🖥️ serverless go function on vercel

--- api/main.go
+++ api/main.go
@@ -23,6 +23,10 @@ type nric struct {
 var nrics = []nric{}
 var dbConnStr string
 
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+}
+
 func main() {
 
 	err := godotenv.Load()
